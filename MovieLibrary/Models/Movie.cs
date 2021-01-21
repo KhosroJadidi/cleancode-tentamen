c53e3070063f85b1073e5d954ad5b7f91464a15d
Khosro <Jadidi>
2021-01-21 09:59:16 +0100

    fixed a bug with model names

--- MovieLibrary/Models/Movie.cs
+++ MovieLibrary/Models/Movie.cs
@@ -1,10 +1,17 @@
-﻿namespace MovieLibrary.Models
+﻿using System.Text.Json.Serialization;
+
+namespace MovieLibrary.Models
 
 {
     public class Movie
     {
+        [JsonPropertyName("id")]
         public string Id { get; set; }
+
+        [JsonPropertyName("title")]
         public string Title { get; set; }
+
+        [JsonPropertyName("rated")]
         public string Rated { get; set; }
     }
 }
